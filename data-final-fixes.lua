f6020cc825f6ce213235b5ea476b2972d3ca2a3e
yaim0425 <yaim0425@gmail.com>
2025-10-18 17:21:30 -0500

    Creación e implementación de This_MOD.get_elements

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -22,8 +22,8 @@ function This_MOD.start()
     --- Valores de la referencia
     This_MOD.reference_values()
 
-    -- --- Obtener los elementos
-    -- This_MOD.get_elements()
+    --- Obtener los elementos
+    This_MOD.get_elements()
 
     -- --- Modificar los elementos
     -- for _, spaces in pairs(This_MOD.to_be_processed) do
@@ -83,9 +83,109 @@ end
 
 
 ---------------------------------------------------------------------------
+---[ Cambios del MOD ]---
+---------------------------------------------------------------------------
+
+function This_MOD.get_elements()
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Función para analizar cada entidad
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    local function valide_recipe(recipe)
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        --- Validación
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        --- Validar el tipo
+        if recipe.type ~= "recipe" then return end
+        if GMOD.has_id(recipe, "d12b") then return end
+        if GMOD.get_tables(This_MOD.to_be_processed, nil, recipe) then return end
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        --- Valores a usar
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        local Ingredient = GMOD.items[recipe.results[1].name]
+        local Result = GMOD.items[recipe.ingredients[1].name]
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        --- Valores para el proceso
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        local Space
+        Space = GMOD.get_tables(This_MOD.to_be_processed, nil, Ingredient)
+        Space = Space and Space[1] or {}
+
+        if GMOD.has_id(recipe.name, GMOD.d12b.category_do) then
+            Space.recipe_do = recipe
+            Space.item_do = Space.item_do or Result
+            Space.item_undo = Space.item_undo or Ingredient
+        end
+
+        if GMOD.has_id(recipe.name, GMOD.d12b.category_undo) then
+            Space.recipe_undo = recipe
+            Space.item_do = Space.item_do or Ingredient
+            Space.item_undo = Space.item_undo or Result
+        end
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        --- Guardar la información
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        This_MOD.to_be_processed[recipe.type] = This_MOD.to_be_processed[recipe.type] or {}
+        This_MOD.to_be_processed[recipe.type][Space.item_undo] = Space
+        This_MOD.to_be_processed[recipe.type][Space.item_do] = Space
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Preparar los datos a usar
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    for _, recipe in pairs(data.raw.recipe) do
+        valide_recipe(recipe)
+    end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
 ---[ Iniciar el MOD ]---
 ---------------------------------------------------------------------------
 
 This_MOD.start()
 
 ---------------------------------------------------------------------------
+GMOD.var_dump(This_MOD.to_be_processed)
+ERROR()
