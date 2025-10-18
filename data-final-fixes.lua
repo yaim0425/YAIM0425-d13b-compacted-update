b38bf80047c359054ac35f1f970d24385dc1ec64
yaim0425 <yaim0425@gmail.com>
2025-10-18 16:00:32 -0500

    Creación de data-final-fixes.lua

---
+++ data-final-fixes.lua
@@ -0,0 +1,91 @@
+---------------------------------------------------------------------------
+---[ data-final-fixes.lua ]---
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Información del MOD ]---
+---------------------------------------------------------------------------
+
+local This_MOD = GMOD.get_id_and_name()
+if not This_MOD then return end
+GMOD[This_MOD.id] = This_MOD
+
+---------------------------------------------------------------------------
+
+function This_MOD.start()
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Valores de la referencia
+    This_MOD.reference_values()
+
+    -- --- Obtener los elementos
+    -- This_MOD.get_elements()
+
+    -- --- Modificar los elementos
+    -- for _, spaces in pairs(This_MOD.to_be_processed) do
+    --     for _, space in pairs(spaces) do
+    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --         -- --- Crear los elementos
+    --         -- This_MOD.create_item(space)
+    --         -- This_MOD.create_entity(space)
+    --         -- This_MOD.create_recipe(space)
+    --         -- This_MOD.create_tech(space)
+
+    --         --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --     end
+    -- end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+function This_MOD.reference_values()
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Validación
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Contenedor de los elementos que el MOD modoficará
+    This_MOD.to_be_processed = {}
+
+    --- Validar si se cargó antes
+    if This_MOD.setting then return end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Valores de la referencia en todos los MODs
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --- Cargar la configuración
+    This_MOD.setting = GMOD.setting[This_MOD.id] or {}
+
+    --- Indicador del mod
+    This_MOD.indicator = {
+        icon  = GMOD.signal["star"],
+        shift = { 14, -14 }
+    }
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+---------------------------------------------------------------------------
+
+
+
+
+
+---------------------------------------------------------------------------
+---[ Iniciar el MOD ]---
+---------------------------------------------------------------------------
+
+This_MOD.start()
+
+---------------------------------------------------------------------------
